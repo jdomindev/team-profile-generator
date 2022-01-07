3bf3d9513ecf32335f3dbc7cb949eb021bf7b73f
Jose Dominguez <jdomindev@gmail.com>
2022-01-06 23:34:43 -0500

    added index.html to test html code that will eventually be generated

---
+++ index.js
@@ -0,0 +1,30 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
+const questions = [
+  { type: "input", message: "What is the team manager's name?", name: "" },
+  { type: "input", message: "What is the team manager's id?", name: "" },
+  { type: "input", message: "What is the team manager's email?", name: "" },
+  { type: "input", message: "What is the team manager's office number?", name: "" },
+  { type: "list", message: "Which type of team member would you like to add?", name: "", choices:["Engineer", "Intern", "I don't want any more team members"]}
+];
+
+function generateHTML(data) {
+  return ``;
+}
+
+function writeToFile(fileName, data) {
+  fs.appendFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log("README written...")
+  );
+}
+
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    // pass answers into writeToFile function
+    writeToFile("", generateHTML(answers));
+  });
+}
+
+// Function call to initialize app
+init();
