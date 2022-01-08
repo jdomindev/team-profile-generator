c22a007f58075475120a9006b6f82718e9c64d41
Jose2 <jdomindev@gmail.com>
2022-01-08 17:46:04 -0500

    worked on generateHTML function, renderCards function, and added functions for prompts on index.js, also finished making classes for lib

---
+++ generateHTML.js
@@ -0,0 +1,76 @@
+function generateHTML(answers) {
+return `<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+        <title>Team Profile Generator</title>
+    </head>
+    <body>
+        <div class="jumbotron jumbotron-fluid">
+            <div class="container-fluid text-white" style="background-color: #7f7f7f;">
+                <h1 class="display-4 text-center">My Team</h1>
+                <p class="lead text-center">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-4">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-header">
+                            ${answers.nameFull}
+                            ${answers.getRole}
+                        </div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">${answers.id}</li>
+                            <li class="list-group-item">${answers.email}</li>
+                            <li class="list-group-item">${answers.officeNum}</li>
+                        </ul>
+                    </div>
+                </div>
+                ${renderCards(answers)}
+            </div>   
+        </div>   
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+    </body>
+</html>`;
+  }
+
+function renderCards(answers) {
+    switch (answers.teamChoice) {
+        case "Engineer":
+return `<div class="col-4">
+    <div class="card" style="width: 18rem;">
+        <div class="card-header">
+        ${answers.nameFull}
+        ${answers.getRole}
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">${answers.id}</li>
+            <li class="list-group-item">${answers.email}</li>
+            <li class="list-group-item">${answers.github}</li>
+        </ul>
+    </div>
+</div>`
+        case "Intern": 
+return `<div class="col-4">
+    <div class="card" style="width: 18rem;">
+        <div class="card-header">
+        ${answers.nameFull}
+        ${answers.getRole}
+        </div>
+        <ul class="list-group list-group-flush">
+            <li class="list-group-item">${answers.id}</li>
+            <li class="list-group-item">${answers.email}</li>
+            <li class="list-group-item">${answers.school}</li>
+        </ul>
+    </div>
+</div>`
+        case "I don't want any more team members":
+        return ``;
+    }
+}
+
+module.exports = generateHTML
--- index.js
+++ index.js
@@ -1,29 +1,93 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const generateHTML = require("./generateHTML")
 
 const questions = [
-  { type: "input", message: "What is the team manager's name?", name: "" },
-  { type: "input", message: "What is the team manager's id?", name: "" },
-  { type: "input", message: "What is the team manager's email?", name: "" },
-  { type: "input", message: "What is the team manager's office number?", name: "" },
-  { type: "list", message: "Which type of team member would you like to add?", name: "", choices:["Engineer", "Intern", "I don't want any more team members"]}
+  // {message : "Welcome to the Team Profile Generator. Please fill in details for the members of your team." },
+  { type: "input", message: "What is the team manager's name?", name: "nameFull" },
+  { type: "input", message: "What is the team manager's id?", name: "id" },
+  { type: "input", message: "What is the team manager's email?", name: "email" },
+  { type: "input", message: "What is the team manager's office number?", name: "officeNum" },
+  { type: "list", message: "Which type of team member would you like to add?", name: "teamChoice", choices:["Engineer", "Intern", "I don't want any more team members"]}
 ];
 
-function generateHTML(data) {
-  return ``;
-}
+const engineerQuestions = [
+  { type: "input", message: "What is the engineer's name?", name: "nameFull" },
+  { type: "input", message: "What is the engineer's id?", name: "id" },
+  { type: "input", message: "What is the engineer's email?", name: "email" },
+  { type: "input", message: "What is the engineer's github username?", name: "github" },
+  { type: "list", message: "Which type of team member would you like to add?", name: "teamChoice", choices:["Engineer", "Intern", "I don't want any more team members"]}
+];
+
+const internQuestions = [
+  { type: "input", message: "What is the intern's name?", name: "nameFull" },
+  { type: "input", message: "What is the intern's id?", name: "id" },
+  { type: "input", message: "What is the intern's email?", name: "email" },
+  { type: "input", message: "What is the intern's school?", name: "school" },
+  { type: "list", message: "Which type of team member would you like to add?", name: "teamChoice", choices:["Engineer", "Intern", "I don't want any more team members"]}
+];
 
 function writeToFile(fileName, data) {
   fs.appendFile(fileName, data, (err) =>
-    err ? console.error(err) : console.log("README written...")
+    err ? console.error(err) : console.log("Team generated...")
   );
 }
 
+function engineerPrompt() {
+  inquirer
+    .prompt(engineerQuestions)
+    .then((answers) =>
+    {
+      switch (answers.teamChoice) {
+        case "Engineer":
+          engineerPrompt()
+          break;
+        case "Intern":
+          internPrompt()
+          break;
+        case "I don't want any more team members":
+          writeToFile("index1.html", generateHTML(answers));
+          break;
+      }
+    });
+}
+
+function internPrompt() {
+  inquirer
+    .prompt(internQuestions)
+    .then((answers) =>
+    {
+      switch (answers.teamChoice) {
+        case "Engineer":
+          engineerPrompt()
+          break;
+        case "Intern":
+          internPrompt()
+          break;
+        case "I don't want any more team members":
+          writeToFile("index1.html", generateHTML(answers));
+          break;
+      }
+    });
+}
+
 function init() {
-  inquirer.prompt(questions).then((answers) => {
-    // pass answers into writeToFile function
-    writeToFile("", generateHTML(answers));
-  });
+  inquirer
+    .prompt(questions)
+    .then((answers) => {
+      // answers need to pass into classes? 
+      switch (answers.teamChoice) {
+        case "Engineer":
+          engineerPrompt()
+          break;
+        case "Intern":
+          internPrompt()
+          break;
+        case "I don't want any more team members":
+          writeToFile("index1.html", generateHTML(answers));
+          break;
+      }
+    });
 }
 
 // Function call to initialize app
